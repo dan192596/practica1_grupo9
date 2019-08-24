4782de59d35c17620206ce94791a1cc85fa82ddc
Ever <everchicasp@gmail.com>
2019-08-23 22:34:54 -0600

    Se hizo Hotfix de cero 201403532

--- Aplicacion/src/aplicacion/cubo_y_raiz_cubica.java
+++ Aplicacion/src/aplicacion/cubo_y_raiz_cubica.java
@@ -10,20 +10,26 @@ package aplicacion;
  * @author Gary Joan
  */
 public class cubo_y_raiz_cubica {
-       public static int cubo(int numero){
-        return numero*numero*numero;
-    
+
+    public static int cubo(int numero) {
+        return numero * numero * numero;
+
     }
 
-    public int RaizCubica (int numero){
-        return (int)Math.pow((double)numero, 1.0/3.0);
+    public int RaizCubica(int numero) {
+        return (int) Math.pow((double) numero, 1.0 / 3.0);
     }
-    public static int multiplicacion (int numero1, int numero2){
-        return numero1* numero2;
+
+    public static int multiplicacion(int numero1, int numero2) {
+        return numero1 * numero2;
     }
-    
-    public static int division(int numero1, int numero2){
-        return numero1/numero2;
-    
+
+    public int division(int numero1, int numero2) {
+        if (numero2 == 0) {
+            return 0;
+        } else {
+            return numero1 / numero2;
+        }
     }
+
 }
