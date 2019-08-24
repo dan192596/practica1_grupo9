cab9e96637033d35aec33ea23b77fa7b791d165c
bredly <brandonbbal1994@gmail.com>
2019-08-23 22:57:53 -0600

    se agregó función de calculo de área de círculo 201403862

---
+++ Aplicacion/src/aplicacion/area_circulo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion;
+
+
+public class area_circulo {
+
+    public double areaDeCirculo(double radio)
+    {
+        return (3.14159 * radio * radio);
+    }
+
+}
